5fb80d91458101054698549843c4086932005616
Olakareem <olakareemomobolarinwa@gmail.com>
2026-01-05 01:45:25 +0100

    Update

--- src/ui/invisio/OurDifferenceSection.jsx
+++ src/ui/invisio/OurDifferenceSection.jsx
@@ -2,18 +2,18 @@ import Image from "next/image";
 import image from "@/assets/images/inv1.png";
 import image2 from "@/assets/images/inv2.png";
 import H2 from "@/components/Typography";
+import VideoPlayer from "@/components/VideoPlayer";
 
 export default function OurDifferenceSection() {
 return ( 
 <div className="flex flex-col">
 <section id="difference" className="py-5 sm:py-12 "> <div className="flex flex-col lg:flex-row items-center gap-8 lg:gap-12 max-w-[1440px] px-6 sm:px-12 md:px-16 mx-auto">
-{/* Image */} <figure className="rounded-xl overflow-hidden relative w-full lg:w-1/2 h-64 sm:h-80 md:h-96 lg:h-[500px]"> 
-<Image
-         src={image}
-         alt="Man pointing to statistics"
-         fill
-         className="object-cover object-top"
-       /> </figure>
+{/* Image */} <div className="w-1/2 rounded-xl overflow-hidden">
+        <VideoPlayer
+          src="/videos/invisio.mp4"
+          className="w-full h-full object-cover rounded-xl"
+        />
+      </div>
 
     {/* Text Content */}
     <div className="relative w-full lg:w-1/2 flex flex-col items-start gap-4 lg:gap-6">
