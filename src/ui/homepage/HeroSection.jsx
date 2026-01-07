ab8407039b350380012da03e3ab006fd3748a68c
Olakareem <olakareemomobolarinwa@gmail.com>
2026-01-07 09:51:20 +0100

    Update

--- src/ui/homepage/HeroSection.jsx
+++ src/ui/homepage/HeroSection.jsx
@@ -41,7 +41,7 @@ const slides = [
 export default function HeroSection() {
   const [emblaRef, emblaApi] = useEmblaCarousel(
     { loop: true },
-    [Autoplay({ delay: 15000, stopOnInteraction: false })]
+    [Autoplay({ delay: 3000, stopOnInteraction: false })]
   );
 
   const [selectedIndex, setSelectedIndex] = useState(0);
