edc1850214ca26236dcbc352967d9c23c5375042
Olakareem <olakareemomobolarinwa@gmail.com>
2026-01-28 15:51:10 +0100

    updates

--- src/ui/about/PartnersSection.jsx
+++ src/ui/about/PartnersSection.jsx
@@ -9,8 +9,8 @@ return (
 {/* Text Content */} <div className="flex flex-col items-start gap-6 lg:w-1/2"> <H2 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-semibold text-grey-900">
 A Vision for Inclusive Innovation </H2>
       <p className="text-grey-700 text-base sm:text-lg md:text-xl text-justify font-medium">
-        We envision a world where African and emerging market data enriches global healthcare, fostering equitable access to superior health decisions. By expanding to key hubs like Ghana and Kenya, integrating blockchain for secure data sharing, and contributing to drug discovery and clinical research, PBR Life Sciences is committed to representing diverse populations in life sciences advancements, creating a healthier, more inclusive tomorrow for all.
-      </p>
+        We envision a world where African and emerging market data enriches global healthcare, fostering equitable access to superior health decisions.
+        </p>
     </div>
 
     {/* Image */}
