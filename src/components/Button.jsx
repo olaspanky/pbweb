7229fc4b8ac0901cb97a327cf4694024329aa0f5
Olakareem <olakareemomobolarinwa@gmail.com>
2026-01-28 16:11:10 +0100

    updates

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ export default function Button({ content, className }) {
   return (
     <Link
       href={content.to}
-      className={`className="px-8 py-3 bg-sky-500 text-white font-semibold rounded-lg hover:bg-sky-600 transition-colors duration-200 shadow-sm ${className}`}
+      className={`p-4 bg-brand-light-blue text-grey-0 text-center rounded-lg font-semibold inline-block ${className}`}
     >
       {content.label}
     </Link>
